6ead0ab1ddf6da5cabd4d67633a5444225c5d479
deepak <anandhadeepak@gmail.com>
2017-03-31 09:39:14 +0530

    init

---
+++ data/cards.js
@@ -0,0 +1,29 @@
+export default { 
+  categories: [
+    {
+      name: 'pictures',
+      subcategories: [
+        {
+          name: 'fruits',
+          items: [
+            'apple',
+            'orange',
+            'papaya'
+          ]
+        },{
+          name: 'animals',
+          items: [
+            'dog',
+            'cat'
+          ]
+        }
+      ]
+    },
+    {
+      name: 'numbers',
+      subcategories: [
+        {} 
+      ]
+    }
+  ]
+}
---
+++ data/rules.js
@@ -0,0 +1,7 @@
+export default { categories:
+  [ { name: 'pictures',
+    starts_on_day: 10,
+    max_parallel_sets: 5,
+    rate_of_increse_in_sets: 1,
+    rate_of_cards_exchanged: 2 },
+    { name: 'hand_written', starts_on_day: 10, max_parallel_sets: 5 } ] }
